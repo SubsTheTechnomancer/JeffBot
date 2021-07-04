1743fa72dc8b1e80c6efb020be7f91f2a108a4e5
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2021-07-04 12:53:58 +0530

    Added Flagship Bot, changed Jeffbot to accomodate more messagees

---
+++ flagshipbot.py
@@ -0,0 +1,19 @@
+import os
+import discord
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('FLAGSHIP_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    botchannel = discord.utils.get(client.get_all_channels(),name='bot-announce')
+    await botchannel.send(content="Purging <@&859740424881045504> roles for the day...")
+    padikuranda = await botchannel.send(content="Congratulations <@!407265504693059584> for being the Flagship Stedier of the Year!")
+    await padikuranda.add_reaction(random.choice(client.emojis))
+
+client.run(TOKEN)
--- jeffbot.py
+++ jeffbot.py
@@ -19,8 +19,16 @@ async def on_message(message):
     
     jeff_quotes = ['you are POOR i am RICH', 'dei dei Waiste fellow', 'Po da panni', 'i am the JEFF BOY', 'Rascals, you are in lou with a dou']
 
-    if(message.content.find("jeff") != -1 or message.content.find("Jeff") != -1):
-        await message.channel.send(content=random.choice(jeff_quotes),tts=True)
+    seskipnow_emojis = ['<:seksipnow:738364549857673307>','<:GodIsDead:802377987302752266>','<:seskichad:755620465153802320>','<:padikuranda:798197283983982654>','<:TargetEngaged:818493944001855511>','<:NoseyBoi:768846530823847937>']
+
+    if(message.content.find("jeff") != -1 or message.content.find("Jeff") != -1 or message.content.find("JEFF") != -1):
+        await message.channel.send(content=random.choice(jeff_quotes),tts=False)
+
+    if(message.content.find("seski") != -1 or message.content.find("Seski") != -1 or message.content.find("pnow") != -1 or message.content.find("Pnow") != -1):
+        await message.channel.send(content=random.choice(seskipnow_emojis),tts=False)
+
+    if(message.content.find("SHIVOUDITYOW") != -1):
+        await message.channel.send(content='https://www.youtube.com/watch?v=st5AgmryAYU&feature=share',tts=False)
 
 client.run(TOKEN)
 
