5f1ac0c72dfb693258bd011e47f00ac5ad9f968d
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2021-07-04 15:31:52 +0530

    added gitignore otherwise funs

--- flagshipbot.py
+++ flagshipbot.py
@@ -11,6 +11,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    guild = client.guilds[0]
     botchannel = discord.utils.get(client.get_all_channels(),name='bot-announce')
     await botchannel.send(content="Purging <@&859740424881045504> roles for the day...")
     padikuranda = await botchannel.send(content="Congratulations <@!407265504693059584> for being the Flagship Stedier of the Year!")
